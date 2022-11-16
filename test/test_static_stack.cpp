472beaab065f87dc78dcae63c2e3e586f149c564
ivanmzk <ivanmzk@gmail.com>
2022-11-16 19:35:43 +0200

    test

--- test/test_static_stack.cpp
+++ test/test_static_stack.cpp
@@ -11,12 +11,14 @@ struct test_value_type{
     std::size_t a;
     std::size_t b;
     std::size_t c;
+    std::size_t d;
 
     test_value_type() = default;
     test_value_type(std::size_t v):
         a{v},
         b{v},
-        c{v}
+        c{v},
+        d{v}
     {}
     bool operator==(const test_value_type& other)const{
         return a==other.a && b==other.b && c==other.c;
