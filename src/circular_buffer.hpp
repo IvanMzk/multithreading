45e1fdd7831ae66926d60c80bcc2eee5de0307e0
ivanmzk <ivanmzk@gmail.com>
2022-11-22 18:31:06 +0200

    mpmc_circular_buffer

--- src/circular_buffer.hpp
+++ src/circular_buffer.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <atomic>
+#include <mutex>
 
 namespace experimental_multithreading{
 
@@ -53,6 +54,62 @@ private:
     std::array<value_type,N+1> elements_;
     std::atomic<std::size_t> push_index;
     std::atomic<std::size_t> pop_index;
+};
+
+
+template<typename T, std::size_t N>
+class mpmc_circular_buffer
+{
+public:
+    using mutex_type = std::mutex;
+    using size_type = std::size_t;
+    using value_type = T;
+    mpmc_circular_buffer() = default;
+
+    bool try_push(const value_type& v){
+        std::unique_lock<mutex_type> lock{guard};
+        bool is_full{index(push_index+1)==pop_index};
+        if (!is_full){
+            elements_[push_index] = v;
+            push_index.store(index(push_index+1));
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    bool try_pop(value_type& v){
+        std::unique_lock<mutex_type> lock{guard};
+        bool is_empty{push_index == pop_index};
+        if (!is_empty){
+            v = elements_[pop_index];
+            pop_index.store(index(pop_index+1));
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void push(const value_type& v){
+
+    }
+
+    auto pop(){
+        
+    }
+
+    auto size()const{
+        std::unique_lock<mutex_type> lock{guard};
+        return pop_index > push_index ? (N+1+push_index-pop_index) : (push_index - pop_index);
+    }
+
+private:
+    auto index(size_type c){return c%(N+1);}
+
+    std::array<value_type,N+1> elements_;
+    size_type push_index;
+    size_type pop_index;
+    mutex_type guard;
 };
 
 }   //end of namespace experimental_multithreading
